fc8b1591aa899811e9d431fa45349466e6d90072
Rui Ying (Mac) <38958822+ruiying-ocean@users.noreply.github.com>
2023-10-27 15:11:13 +0100

    add `sp_info.R`

---
+++ code/sp_info.R
@@ -0,0 +1,24 @@
+## change species name
+## Add a abbreviation column
+species_abbrev <- function(full_name, sep_string = ". ") {
+  name_parts <- str_split(full_name, " ")[[1]]
+  genus_name <- name_parts[1]
+  species_name <- name_parts[2]
+  
+  if (length(name_parts) > 2) {
+    subspecies_name <- name_parts[3]
+    genus_abbrev <- str_sub(genus_name, 1, 1)
+    abbrev <- paste(genus_abbrev, species_name, sep = sep_string)
+    abbrev <- paste(abbrev, subspecies_name, sep=" ")
+  } else {
+    genus_abbrev <- str_sub(genus_name, 1, 1)
+    abbrev <- paste(genus_abbrev, species_name, sep = sep_string)
+  }
+  
+  return(abbrev)
+}
+
+foram_sp_db <- read_csv("https://raw.githubusercontent.com/ruiying-ocean/lgm_foram_census/main/fg/foram_sp_db.csv")
+
+trait_info <-foram_sp_db %>%
+   mutate(sp = map_vec(Name, species_abbrev)) %>% select(sp, Symbiosis, Spinose)
